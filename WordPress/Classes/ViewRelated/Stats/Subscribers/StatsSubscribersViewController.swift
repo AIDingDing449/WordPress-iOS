63c31a4f34247bfd44c891033d769e9512a6f1cd
AIDingDing449 <AIDingDing449@outlook.com>
2024-04-27 02:10:38 +0000

    Jack of all trades，master of none.

--- WordPress/Classes/ViewRelated/Stats/Subscribers/StatsSubscribersViewController.swift
+++ WordPress/Classes/ViewRelated/Stats/Subscribers/StatsSubscribersViewController.swift
@@ -69,6 +69,7 @@ final class StatsSubscribersViewController: SiteStatsBaseTableViewController {
 
     func tableRowTypes() -> [ImmuTableRow.Type] {
         return [
+            SubscriberChartRow.self,
             TopTotalsPeriodStatsRow.self,
             StatsGhostTopImmutableRow.self,
             StatsErrorRow.self
@@ -78,8 +79,18 @@ final class StatsSubscribersViewController: SiteStatsBaseTableViewController {
 
 extension StatsSubscribersViewController: SiteStatsPeriodDelegate {
     func viewMoreSelectedForStatSection(_ statSection: StatSection) {
-        let detailTableViewController = SiteStatsDetailTableViewController.loadFromStoryboard()
-        detailTableViewController.configure(statSection: statSection)
-        navigationController?.pushViewController(detailTableViewController, animated: true)
+        switch statSection {
+        case .subscribersList:
+            guard let blog = RootViewCoordinator.sharedPresenter.mySitesCoordinator.currentBlog,
+                  let peopleViewController = PeopleViewController.controllerWithBlog(blog, selectedFilter: .followers) else { return }
+            navigationController?.pushViewController(peopleViewController, animated: true)
+        case .subscribersEmailsSummary:
+            let detailTableViewController = SiteStatsDetailTableViewController.loadFromStoryboard()
+            detailTableViewController.configure(statSection: statSection)
+            navigationController?.pushViewController(detailTableViewController, animated: true)
+        default:
+            // TODO
+            DDLogInfo("\(statSection) selected")
+        }
     }
 }
