601391aa0dbebaba2438126949c8c4c7f099d318
AIDingDing449 <AIDingDing449@outlook.com>
2025-09-09 02:53:18 +0000

    0cwl8ta7 Бороо орж байна

--- WordPress/Classes/ViewRelated/Blog/Blog Details/Detail Header/BlogDetailHeaderView.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Details/Detail Header/BlogDetailHeaderView.swift
@@ -220,6 +220,8 @@ extension BlogDetailHeaderView {
             }
             configuration.contentInsets = isSidebarModeEnabled ? NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 2, trailing: 0) : NSDirectionalEdgeInsets(top: 2, leading: 0, bottom: 1, trailing: 0)
             configuration.titleLineBreakMode = .byTruncatingTail
+            configuration.cornerStyle = .fixed
+            configuration.background.cornerRadius = 0
             button.configuration = configuration
             button.accessibilityHint = NSLocalizedString("Tap to view your site", comment: "Accessibility hint for button used to view the user's site")
             button.translatesAutoresizingMaskIntoConstraints = false
@@ -299,15 +301,7 @@ extension BlogDetailHeaderView {
 
         fileprivate func configureLargeTitleMode() {
             siteIconView.setImageViewSize(50)
-
             siteIconView.transform = CGAffineTransform(translationX: 0, y: 2) // Visually center vertically
-
-            siteSwitcherButton.removeFromSuperview()
-            mainStackView.addSubview(siteSwitcherButton)
-            NSLayoutConstraint.activate([
-                siteSwitcherButton.lastBaselineAnchor.constraint(equalTo: titleButton.lastBaselineAnchor, constant: -2),
-                siteSwitcherButton.leadingAnchor.constraint(equalTo: titleButton.trailingAnchor, constant: 2)
-            ])
         }
 
         func set(url: String) {
