c6e67d6bd4c944859783a5dc57001dfe462e1da3
AIDingDing449 <AIDingDing449@outlook.com>
2025-11-04 03:02:22 +0000

    clwkcsef Бороо орж байна

--- WordPress/Classes/ViewRelated/Blog/Site Settings/SiteSettingsViewController+Swift.swift
+++ WordPress/Classes/ViewRelated/Blog/Site Settings/SiteSettingsViewController+Swift.swift
@@ -13,11 +13,11 @@ extension SiteSettingsViewController {
     @objc public func showPrivacySelector() {
         struct SiteSettingsPrivacyPicker: View {
             let blog: Blog
-            @State var selection: SiteVisibility
-            let onChange: (SiteVisibility) -> Void
+            @State var selection: WordPress.SiteVisibility
+            let onChange: (WordPress.SiteVisibility) -> Void
 
             var body: some View {
-                SettingsPickerListView(selection: $selection, values: SiteVisibility.eligiblePickerValues(for: blog))
+                SettingsPickerListView(selection: $selection, values: WordPress.SiteVisibility.eligiblePickerValues(for: blog))
                     .onChange(of: selection, perform: onChange)
             }
         }
@@ -58,7 +58,7 @@ extension SiteSettingsViewController {
     @objc public func showCustomTaxonomies() {
         let viewController: UIViewController
         if let client = try? WordPressClient(site: .init(blog: blog)) {
-            let rootView = SiteCustomTaxonomiesView(blog: self.blog, api: client.api)
+            let rootView = SiteCustomTaxonomiesView(blog: self.blog, client: client)
             viewController = UIHostingController(rootView: rootView)
         } else {
             let feature = NSLocalizedString(
@@ -67,7 +67,7 @@ extension SiteSettingsViewController {
                 comment: "Feature name for managing terms and taxonomies in the app"
             )
             let rootView = ApplicationPasswordRequiredView(blog: self.blog, localizedFeatureName: feature, presentingViewController: self) { client in
-                SiteCustomTaxonomiesView(blog: self.blog, api: client.api)
+                SiteCustomTaxonomiesView(blog: self.blog, client: client)
             }
             viewController = UIHostingController(rootView: rootView)
         }
