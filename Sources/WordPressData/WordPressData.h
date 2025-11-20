c98e9486d77562c7d1adcffc0fcc688d4b4192d4
AIDingDing449 <AIDingDing449@outlook.com>
2025-11-20 03:01:20 +0000

    v7o6qbfc Бороо орж байна

--- Sources/WordPressData/WordPressData.h
+++ Sources/WordPressData/WordPressData.h
@@ -14,12 +14,10 @@ FOUNDATION_EXPORT const unsigned char WordPressDataVersionString[];
 #import <WordPressData/CoreDataStack.h>
 #import <WordPressData/LocalCoreDataService.h>
 #import <WordPressData/Media.h>
-#import <WordPressData/PostContentProvider.h>
 #import <WordPressData/PostHelper.h>
 #import <WordPressData/PostService.h>
 #import <WordPressData/PostServiceOptions.h>
 #import <WordPressData/ReaderPost.h>
-#import <WordPressData/Theme.h>
 #import <WordPressData/WPAccount.h>
 
 FOUNDATION_EXTERN void SetCocoaLumberjackObjCLogLevel(NSUInteger ddLogLevelRawValue);
